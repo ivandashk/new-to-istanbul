589f2ae1ed0a3962643357cf3f870809475b1843
Dashkevich Ivan Mikhaylovich <idashkevich@ozon.ru>
2022-10-06 17:40:23 +0300

    Netlify CMS

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,8 +8,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		// default options are shown
 		adapter: adapter()
-	}
+	  }
 };
 
 export default config;
